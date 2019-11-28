cc0f692bf792405576c588378e4eca2951e35db3
Tamara Nelson-Fromm <tln2@illinois.edu>
2019-11-28 14:48:43 -0600

    its a graph

--- visualization.js
+++ visualization.js
@@ -1,49 +1,70 @@
 // read in data and call visualize
-// $(function() {
-//   d3.json("CSMap.JSON").then(function(data) {
-//     visualize(data);
-//   })
-// })
-
-var data = {
-  nodes: [{
-    name: "CS 100"
-  },
-  {
-    name: "ENG 100"
-  },
-  {
-    name: "MATH 221"
-  }],
-  edges: [{
-    source: "CS 100",
-    target: "ENG 100",
-    label: "Computer Science"
-  },
-  {
-    source: "ENG 100",
-    target: "MATH 221",
-    label: "Computer Science"
-  }]
-}
+$(function() {
+  d3.json("CSMap.JSON").then(function(data) {
+    visualize(data);
+  })
+})
 
 var visualize = function(data) {
   console.log(data);
 
-  var margin = { top: 50, right: 50, bottom: 50, left: 50 };
-  var width = 960 - margin.left - margin.right;
-  var height = 960 - margin.top - margin.bottom;
+  var vis = d3.select('#chart').attr('transform', 'translate(20, 20)');
+  var links = vis.selectAll('.link').data(data.links);
+  var nodes = vis.selectAll('.node').data(data.nodes);
 
-  var svg = d3.select("#chart")
-  .append("svg")
-  .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom)
-  .style("width", width + margin.left + margin.right)
-  .style("height", height + margin.top + margin.bottom)
+  // Store nodes in a hash by name
+  var nodesByName = {};
+  // why do these .each()s need .enter() before them??
+  nodes.enter().each(function(d) {
+    nodesByName[d.name] = d;
+  });
 
-  var g = svg.append("g")
-  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  // Convert link references to objects
+  links.enter().each(function(link) {
+      link.source = nodesByName[link.source];
+      link.target = nodesByName[link.target];
+      if (!link.source.links) {
+          link.source.links = [];
+      }
+      link.source.links.push(link.target);
+      if (!link.target.links) {
+          link.target.links = [];
+      }
+      link.target.links.push(link.source);
+  });
 
-  // create graph edges
-  
+  // Compute positions based on distance from root
+  var setPosition = function(node, i, j, depth) {
+      if (!depth) {
+        depth = 0;
+      }
+
+      if (!node.x) {
+          node.x = (i + 1) * 40;
+          node.y = (depth + 1) * 40;
+          if (depth <= 1) {
+              node.links.forEach(function(d, i2) {
+                  setPosition(d, i2, j, depth + 1);
+              });
+          }
+
+      }
+  };
+  nodes.enter().each(setPosition);
+
+  links.enter().append('line').attr('class', 'link').attr('x1', function(d) {
+    return d.source.x;
+  }).attr('y1', function(d) {
+    return d.source.y;
+  }).attr('x2', function(d) {
+    return d.target.x;
+  }).attr('y2', function(d) {
+    return d.target.y;
+  });
+
+  nodes.enter().append('g').attr('class', 'node').append('circle').attr('r', 5).attr('transform', function(d) {
+    return 'translate(' + d.x + ', ' + d.y + ')';
+  }).append('title').text(function(d) {
+      return d.name;
+  });
 }

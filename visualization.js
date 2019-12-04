3d8a716e4a133209f93e568cbd39fe8d760384ae
Tamara Nelson-Fromm <tln2@illinois.edu>
2019-12-03 20:10:05 -0600

    layered cocurrent lines

--- visualization.js
+++ visualization.js
@@ -10,7 +10,19 @@ $(function() {
 var visualize = function(data, colors) {
   console.log(data);
 
-  var vis = d3.select('#chart').append("svg").attr('transform', 'translate(20, 20)').append("g");
+  // boilerplate setup
+  var margin = { top: 50, right: 50, bottom: 50, left: 50 },
+     width = 960 - margin.left - margin.right,
+     height = 1080 - margin.top - margin.bottom;
+
+  var vis = d3.select('#chart')
+  .append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .style("width", width + margin.left + margin.right)
+  .style("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
   var edges = vis.selectAll('.edge').data(data.edges);
   var nodes = vis.selectAll('.node').data(data.nodes);
 
@@ -25,15 +37,15 @@ var visualize = function(data, colors) {
     edge.source = nodesDict[edge.source];
     edge.target = nodesDict[edge.target];
 
-    if (!edge.source.edges) {
-      edge.source.edges = [];
+    if (!edge.source.targetEdges) {
+      edge.source.targetEdges = [];
     }
-    edge.source.edges.push(edge.target);
+    edge.source.targetEdges.push(edge);
 
-    if (!edge.target.edges) {
-      edge.target.edges = [];
+    if (!edge.target.sourceEdges) {
+      edge.target.sourceEdges = [];
     }
-    edge.target.edges.push(edge.source);
+    edge.target.sourceEdges.push(edge);
   });
 
   // Compute positions based for nodes based on the major they are a part of
@@ -47,7 +59,47 @@ var visualize = function(data, colors) {
     }
   }
 
+  // when there are multiple edges between the same two nodes, assign each edge a number
+  edges.enter().each(function(d) {
+    if (d.source.targetEdges) {
+      let sharedEdges = d.source.targetEdges.filter(element => element.target == d.target);
+
+      if (sharedEdges.length > 1) {
+        let coLines = sharedEdges.length;
+        let rank = sharedEdges.findIndex(element => element.label == d.label) + 1;
+        let center = Math.ceil(coLines / 2);
+
+        if ((coLines % 2) == 0) { // even case
+          // for case of 2 lines: rank 1 should be 2 pixels above center, rank 2 be 2 pixels below center
+          // for case of 4 lines: rank 1 5 pixels above, rank 2 should be 2 pixels above center, rank 3 be 2 pixels below center, rank 4 5 pixels below
+          // for case of 6: 8, 5, 2; 2, 5, 8
+          if (rank <= center) {
+            d.y1 = d.source.y + (2 + (3 * (center - rank)));
+            d.y2 = d.target.y + (2 + (3 * (center - rank)));
+          } else {
+            d.y1 = d.source.y - (2 + (3 * (rank - center - 1)));
+            d.y2 = d.target.y - (2 + (3 * (rank - center - 1)));
+          }
+        } else { // odd case
+          // for case of 3 lines: rank 1 should be 3 pixels above center, rank 2 should be on center, rank 3 should be 3 pixels below center
+          if (rank < center) {
+            d.y1 = d.source.y + (3 * (center - rank));
+            d.y2 = d.target.y + (3 * (center - rank));
+          }
+
+          if (rank > center) {
+            d.y1 = d.source.y - (3 * (rank - center));
+            d.y2 = d.target.y - (3 * (rank - center));
+          }
+        }
+      }
+    }
+  });
+
+  // TODO: all above needs to exist before rendering -- better way for that?
+
   // simple tooltip to display node's name
+  // TODO: fix
   var tip = d3.tip()
   .attr('class', 'd3-tip')
   .offset([-10, 0])
@@ -58,13 +110,33 @@ var visualize = function(data, colors) {
   vis.call(tip);
 
   // render edges
-  edges.enter().append('line').attr('class', 'edge').attr('x1', function(d) {
+  edges.enter().append('line').attr('class', 'edge')
+  .attr('x1', function(d) {
+    // if (d.x1) {
+    //   return d.x1;
+    // }
+
     return d.source.x;
-  }).attr('y1', function(d) {
+  })
+  .attr('y1', function(d) {
+    if (d.y1) {
+      return d.y1;
+    }
+
     return d.source.y;
-  }).attr('x2', function(d) {
+  })
+  .attr('x2', function(d) {
+    // if (d.x2) {
+    //   return d.x2;
+    // }
+
     return d.target.x;
-  }).attr('y2', function(d) {
+  })
+  .attr('y2', function(d) {
+    if (d.y2) {
+      return d.y2;
+    }
+
     return d.target.y;
   })
   .attr('stroke', function(d) { // make edge color the major's color
@@ -78,7 +150,21 @@ var visualize = function(data, colors) {
   });
 
   // render nodes
-  nodes.enter().append('g').attr('class', 'node').append('circle').attr('r', 5).attr('transform', function(d) {
+  nodes.enter().append('g').attr('class', 'node').append('circle')
+  .attr('r', function(d) { // change size according to number of edges, default 5
+    if (d.targetEdges && d.sourceEdges) {
+      let num = (d.targetEdges.length >= d.sourceEdges.length) ? d.targetEdges.length : d.sourceEdges.length;
+
+      if (num <= 2) {
+        return 5;
+      }
+
+      return 5 + (num / 2); // TODO: optomize this
+    }
+
+    return 5;
+  })
+  .attr('transform', function(d) {
     return 'translate(' + d.x + ', ' + d.y + ')';
   }).append('title').text(function(d) {
       return d.name;

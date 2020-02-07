e3091d97d0a3d90c2ba518649ca54a7729a98bf6
Tamara Nelson-Fromm <tamaranf7@gmail.com>
2020-02-06 23:53:13 -0600

    criteria finished

--- generate-graph/filestograph.py
+++ generate-graph/filestograph.py
@@ -67,10 +67,6 @@ class major:
         self.courses = courses
 
 
-# list to save edges, dictionary to save nodes
-edges = []
-nodes = {}
-
 # create graph
 G = nx.Graph()
 
@@ -122,25 +118,37 @@ for entry in os.scandir("majors"):
             if not G.has_node(courses[x]): # add node to graph
                 G.add_node(courses[x])
 
-            if courses[x] not in nodes: # add node to dictionary
-                nodes[courses[x]] = node(courses[x])
-
-            if x != (len(courses) - 1): # add edges to edge list and to graph
+            if x != (len(courses) - 1): # add edges to graph
                 G.add_edge(courses[x], courses[x + 1], major=major_title)
-                edges.append(edge(courses[x], courses[x + 1], major_title))
 
 # graph drawing part:
+
+def calc_smallest_angle(x1, y1, x2, y2, x3, y3): # find the smallest angle between meeting line segments where x2, y2 is the vertex
+    a = np.array([x1, y1])
+    b = np.array([x2, y2])
+    c = np.array([x3, y3])
+
+    ab = b - a
+    bc = c - b
+    cb = b - c
+    ba = a - b
+
+    angle1 = math.acos(np.dot(ab, bc) / (np.linalg.norm(ab) * np.linalg.norm(bc)))
+    angle2 = math.acos(np.dot(cb, ba) / (np.linalg.norm(cb) * np.linalg.norm(ba)))
+
+    if angle1 < angle2:
+        return angle1
+
+    return angle2
+
 
 def angularResolution_calc(n, G):
     # neighbors of input node
     E = G.neighbors(n)
 
     sum = 0
-    for i in range(0, len(E) - 1):
-        a = math.hypot(G[E[i]]]['x'] - G[E[i + 1]]['x'],  G[E[i]]['y'] - G[E[i + 1]]['y']) # c to b
-        b = math.hypot(G[n]['x'] - G[E[i]]['x'], G[n]['y'] - G[E[i]]['y']) # a to c
-        c = math.hypot(G[n]['x'] - G[E[i + 1]]['x'], G[n]['y'] - G[E[i + 1]]['y']) # a to b
-        angle = math.acos((b**2 + c**2 - a**2) / (2*b*c))
+    for i in range(0, len(E) - 1): # between i and i + 1
+        angle = calc_smallest_angle(G[E[i]]['x'], G[E[i]]['y'], G[n]['x'], G[n]['y'], G[E[i + 1]]['x'], G[E[i + 1]]['y'])
         sum += abs(((2*math.pi) / len(E)) - angle)
     return sum
 
@@ -174,11 +182,45 @@ def balancedEdgeLength(G): # penalizing stations with degree two that have incid
 
 
 def lineStraightness(G): # Edges that form part of a line should, where possible, be collinear either side of each station that the line passes through
-    return 0
+    sum = 0
+    for n in G.nodes():
+        # get edges of n
+        inner_sum = 0
+        E = G.edges(n, data=True)
+
+        if (len(E) > 1):
+            # get majors that go through n
+            lines = [i[2]['major'] for i in E]
+
+            # iterate through majors, isolate the edges of that major, and perform the angle calculation
+            for l in lines:
+                e12 = [i for i in E if i[2]['major'] == l]
+                if len(e12 == 2): # not a terminating node for this line
+                    n1 = ""
+                    n2 = ""
+
+                    if e12[0][0] != n:
+                        n1 = e12[0][0]
+                    else:
+                        n1 = e12[0][1]
+
+                    if e12[1][0] != n:
+                        n2 = e12[1][0]
+                    else:
+                        n2 = e12[1][1]
+
+                    inner_sum += calc_smallest_angle(G[n1]['x'], G[n1]['y'], G[n]['x'], G[n]['y'], G[n2]['x'], G[n2]['y'])
+
+        sum += inner_sum
+
+    return sum
 
 
 def octilinearity(G): # Each edge should be drawn horizontally, vertically, or diagonally at 45 degree
-    return 0
+    sum = 0
+    for e in G.edges():
+        sum += math.abs(math.sin(4 * math.atan(math.abs(G[e[0]]['y'] - G[e[1]]['y']) / math.abs(G[e[0]]['x'] - G[e[1]]['x']))))
+    return sum
 
 
 def calcStationCriteria(G): # The criteria evaluate to a lower value when improved
@@ -210,11 +252,19 @@ def moveCluster(p):
 
 
 # create an initial layout -- assign x and y coordinates to graph on a grid
-width = G.number_of_nodes() / 4
 height = 4
+width = math.floor(G.number_of_nodes() / (height - 1))
 pos = {}
+x = 1
+y = 1
 for n in G.nodes():
-    pos[n] = (0, 0)
+    pos[n] = (x, y)
+
+    if (x == width):
+        x = 1
+        y += 1
+    else:
+        x += 1
 
 # calculate initial layout fitness
 mto = calcStationCriteria(G)

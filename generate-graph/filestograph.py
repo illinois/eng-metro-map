3bbb98d2ad370f2ebc64778ac0b555a1fc331e93
Tamara Nelson-Fromm <tamaranf7@gmail.com>
2020-03-25 21:27:03 -0500

    ready to run

--- generate-graph/filestograph.py
+++ generate-graph/filestograph.py
@@ -2,6 +2,7 @@ import os
 import re
 import pandas as pd
 import json
+import datetime
 from nongeosubwaygenerator import *
 
 def containsCourseCodes(st):
@@ -14,7 +15,7 @@ def sort_courses_by_prereqs(courses, prereqs):
         changes_made = False
 
         for x in range(0, len(courses)):
-            if courses[x][0] == "(":
+            if isinstance(courses[x], list):
                 continue
 
             if (courses[x] not in prereqs) or (len(prereqs[courses[x]]) == 0):
@@ -48,7 +49,7 @@ def create_major_csv(): # TODO
     pass
 
 
-def read_in_files(foldr_name, prereqs): # TODO: fix for file format
+def read_in_files(foldr_name): # TODO: fix for file format
     G = nx.MultiGraph()
 
     for entry in os.scandir(foldr_name):
@@ -68,7 +69,7 @@ def read_in_files(foldr_name, prereqs): # TODO: fix for file format
                     elif "AND" in line: # if AND add both in order
                         courses.extend(c)
                     else: # if OR save in list as "(CODE, CODE)"
-                        courses.append("(" + c[0] + ", " + c[1] +")")
+                        courses.append(c)
 
                 if "(" in line:
                     parentheses += line.count("(")
@@ -85,17 +86,33 @@ def read_in_files(foldr_name, prereqs): # TODO: fix for file format
                     parentheses -= line.count(")")
 
             # sort courses by prereq
-            courses = sort_courses_by_prereqs(courses, prereqs)
+            # courses = sort_courses_by_prereqs(courses, prereqs)
             print(major_title)
             print(courses)
 
             # add each course to nodes (if dne), and add an edge to the next node
             for x in range(0, len(courses)):
-                if not G.has_node(courses[x]): # add node to graph
-                    G.add_node(courses[x])
+                if isinstance(courses[x], list):
+                    for c in courses[x]:
+                        if not G.has_node(c): # add node to graph
+                            G.add_node(c)
 
-                if x != (len(courses) - 1): # add edges to graph
-                    G.add_edge(courses[x], courses[x + 1], major=major_title)
+                        if x != (len(courses) - 1): # add edges to graph
+                            if isinstance(courses[x + 1], list):
+                                for cnext in courses[x + 1]:
+                                    G.add_edge(c, cnext, major=major_title)
+                            else:
+                                G.add_edge(c, courses[x + 1], major=major_title)
+                else:
+                    if not G.has_node(courses[x]): # add node to graph
+                        G.add_node(courses[x])
+
+                    if x != (len(courses) - 1): # add edges to graph
+                        if isinstance(courses[x + 1], list):
+                            for cnext in courses[x + 1]:
+                                G.add_edge(courses[x], cnext, major=major_title)
+                        else:
+                            G.add_edge(courses[x], courses[x + 1], major=major_title)
 
     return G
 
@@ -103,18 +120,18 @@ def read_in_files(foldr_name, prereqs): # TODO: fix for file format
 # TODO: make this a function I can call from terminal where I can specify folder name, final file name, scale, radius
 
 # load in prereqs csv from https://github.com/illinois/prerequisites-dataset (so we only do this once)
-prereqs = {}
-prereq_table = pd.read_csv("uiuc-prerequisites.csv", header = 0)
-for x in range(0, len(prereq_table.index)):
-    prereqs[prereq_table.loc[x, 'Course']] = []
-    for y in range(0, int(prereq_table.loc[x, 'PrerequisiteNumber'])):
-        prereqs[prereq_table.loc[x, 'Course']].append(prereq_table.loc[x, str(y)])
+# prereqs = {}
+# prereq_table = pd.read_csv("uiuc-prerequisites.csv", header = 0)
+# for x in range(0, len(prereq_table.index)):
+#     prereqs[prereq_table.loc[x, 'Course']] = []
+#     for y in range(0, int(prereq_table.loc[x, 'PrerequisiteNumber'])):
+#         prereqs[prereq_table.loc[x, 'Course']].append(prereq_table.loc[x, str(y)])
 
-G = read_in_files("CoE", prereqs)
+G = read_in_files("majors")
 
 # run coordinate algoritm
 G = assign_coordinates(G, 1000, 100)
 
 # create json file from graph
 json = json.dumps(json_graph.node_link_data(G))
-open("graph.json", "w").write(json)
+open((str(datetime.datetime.now().time()) + "graph.json"), "w").write(json)
